fdec84c3aeaba89805cc1beb527f2278d2209669
SMARTurbanism101 <85216262+urbanism101@users.noreply.github.com>
2025-02-24 15:46:23 +0900

    Update main_message.js

--- components/main_message.js
+++ components/main_message.js
@@ -1,8 +1,16 @@
 export const weddingMessage = `
-  <section>
-    <p class="fade-line"><span style="font-family: 'NanumDaHaengCe'">서로가 그동안 다른 길을 걷다</span></p>
-    <p class="fade-line"><span style="font-family: 'NanumDaHaengCe'">같은 방향으로 걸음을 딛게 되었습니다</span></p>
-    <p class="fade-line"><span style="font-family: 'NanumDaHaengCe'">귀한 시간 내주셔서 저희의 앞날을</span></p>
-    <p class="fade-line"><span style="font-family: 'NanumDaHaengCe'">응원해주시면 감사하겠습니다</span></p>
+  <section style="font-size: 24px; line-height: 1.6;">
+    <p class="fade-line" style="margin: 20px 0;">
+      <span style="font-family: 'NanumDaHaengCe';">서로가 그동안 다른 길을 걷다</span>
+    </p>
+    <p class="fade-line" style="margin: 20px 0;">
+      <span style="font-family: 'NanumDaHaengCe';">같은 방향으로 걸음을 딛게 되었습니다</span>
+    </p>
+    <p class="fade-line" style="margin: 20px 0;">
+      <span style="font-family: 'NanumDaHaengCe';">귀한 시간 내주셔서 저희의 앞날을</span>
+    </p>
+    <p class="fade-line" style="margin: 20px 0;">
+      <span style="font-family: 'NanumDaHaengCe';">응원해주시면 감사하겠습니다</span>
+    </p>
   </section>
 `;
